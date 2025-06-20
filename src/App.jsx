7c74732cf1195fcd007c5d6e8d3b1f78c4f044e9
vineet murjani <153760405+murjanii@users.noreply.github.com>
2025-06-20 10:48:02 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import Hero from "./Hero.jsx";
 import Navbar from './Navbar.jsx';
 import ImageGrid from './ImageGrid.jsx';
-import PopularSection from './Popularsection.jsx';
+import PopularSection from './PopularSection.jsx';
 import Footer from './Footer.jsx';
 
 
